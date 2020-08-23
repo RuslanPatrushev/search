f256e6964a04c55e07850cf1006368ebd965d470
Rps 23 <rusleshkin@gmail.com>
2020-08-23 11:48:54 +0300

    edited style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Book, GoogleBookService} from "./services/google-book.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'search-app';
+  userName: string = '';
+  response: Book[];
+
+  constructor(private http: GoogleBookService) {
+  }
+
+  search(query: string) {
+    if (query) {
+      console.log(query);
+      this.http.search(query).subscribe(
+        (data) => {
+          console.log(data);
+          this.response = data;
+          console.log('fe', this.response);
+        }, (eror) => {
+          console.log(eror);
+        }, () => {
+          console.log('completed')
+        }
+      )
+    } else{
+      this.response = null;
+    }
+  }
 }
